7f3f80aaf0df7322117d0e461dd59efee30b601b
iwnuplylo <78899891+IwnuplyNotTyan@users.noreply.github.com>
2024-11-08 17:07:25 +0200

    Add theme support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple. Fast. Beautiful. - Koi
 
-*23 lines of code, 3 libery, easy to use*
+*24 lines of code, 3 libery, easy to use*
 
 ![koi](https://github.com/IwnuplyNotTyan/koi/blob/main/image/Showcase.png)
 
@@ -8,6 +8,11 @@
 koi <example.md>
 ```
 
+## Setting
+```bash
+export THEME=dark
+```
+Must set to dark, light, notty or dracula
 ## Build from source
 
 ``` bash
--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func main() {
+ theme := os.Getenv("THEME")
  if len(os.Args) < 2 {
 	 return
  }
  filename := os.Args[1]
-
  content, err := os.ReadFile(filename)
  if err != nil {
 	 fmt.Println("read file error")
 	 return
  }
- render, err := glamour.Render(string(content), "dark")
+ render, err := glamour.Render(string(content), theme)
  if err != nil {
 	 fmt.Println("render error")
  	 return

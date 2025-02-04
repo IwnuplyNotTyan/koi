28a867c20a0c1c7385823a5229108fa7877d009f
iwnuplynottyan <wolflast21@gmail.com>
2025-02-05 00:16:16 +0200

    auto dark theme select if nil

--- main.go
+++ main.go
@@ -3,24 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"github.com/charmbracelet/glamour"
 )
 
 func main() {
- theme := os.Getenv("THEME")
- if len(os.Args) < 2 {
-	 return
- }
- filename := os.Args[1]
- content, err := os.ReadFile(filename)
- if err != nil {
-	 fmt.Println("read file error")
-	 return
- }
- render, err := glamour.Render(string(content), theme)
- if err != nil {
-	 fmt.Println("render error")
- 	 return
- }
- fmt.Print(render)
+	theme := os.Getenv("THEME")
+	if theme == "" {
+		theme = "dark"
+	}
+	if len(os.Args) < 2 {
+		return
+	}
+	filename := os.Args[1]
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("read file error")
+		return
+	}
+	render, err := glamour.Render(string(content), theme)
+	if err != nil {
+		fmt.Println("render error")
+		return
+	}
+	fmt.Print(render)
 }

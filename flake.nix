52b5ad5b390124c9613e1905428e000058ac2818
IwnuplyNotTyan <wolflast21@gmail.com>
2026-05-30 22:11:58 +0300

    feat: hm options

--- flake.nix
+++ flake.nix
@@ -23,9 +23,9 @@
         {
           default = pkgs.buildGoModule {
             pname = "koi";
-            version = "0.1.0";
+            version = "0.2.0";
             src = self;
-	    modules = ./gomod2nix.toml;
+            modules = ./gomod2nix.toml;
 
             vendorHash = "sha256-eq8wtckFoXpwubNpD/RtkylC0Uyg8j40MeOlJELFOs8=";
 
@@ -36,6 +36,33 @@
             };
           };
         });
+
+      homeManagerModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.koi;
+        in
+        {
+          options.programs.koi = {
+            enable = lib.mkEnableOption "koi markdown reader";
+
+            theme = lib.mkOption {
+              type = lib.types.enum [ "dark" "light" "notty" "dracula" ];
+              default = "dark";
+              description = ''
+                Default theme for koi. Sets the KOI_DEFAULT_THEME environment variable.
+                Possible values: dark, light, notty, dracula.
+              '';
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            home.packages = [ self.packages.${pkgs.system}.default ];
+
+            home.sessionVariables = {
+              KOI_DEFAULT_THEME = cfg.theme;
+            };
+          };
+        };
 
       devShells = forAllSystems (system:
         let
